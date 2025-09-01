d4a4709d8639af9e0772896adc4110122313d7c8
szep <szepiet33@gmail.com>
2025-09-01 16:51:49 +0200

    extract details button into separate component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 export default defineConfig({
   integrations: [react()],
   vite: {
