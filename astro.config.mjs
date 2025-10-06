5c6d15f94813e172150a9beef94797ccfff921c1
szep <szepiet33@gmail.com>
2025-10-06 17:50:00 +0200

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
-<<<<<<< HEAD
 import react from '@astrojs/react';
-=======
-
->>>>>>> 19f05df8774ffbfd0c650263304befaf12254a9c
 export default defineConfig({
   integrations: [react()],
   vite: {
